62f7ba8b98b439bb0f7f7a43b49c6391d7906b66
tcoots88 <travis.coots@yahoo.com>
2019-12-16 16:36:48 -0800

    final

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 'use strict'
-angular.module 'app', []
 
-.directive 'countdown', ['Util', '$interval', (Util, $interval) ->
-  restrict: 'A'
-  scope:
-    date: '@'
-  link: (scope, element) ->    
-    future = new Date scope.date
-    $interval ->
-      diff = Math.floor (future.getTime() - new Date().getTime()) / 1000
-      element.text  Util.dhms diff
-    , 1000
-    return
-]
 
-.factory 'Util', [ ->
-  {
-    dhms: (t) ->
-      days = Math.floor t / 86400
-      t -= days * 86400
-      hours = Math.floor(t / 3600) % 24
-      t -= hours * 3600
-      minutes = Math.floor(t / 60) % 60
-      t -= minutes * 60
-      seconds = t % 60
-      [ days + 'd', hours + 'h', minutes + 'm', seconds + 's' ].join ' '
-  }
-]
+var deadline = 'Feb 12 2020 00:00:01 GMT-0500';
+function time_remaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+}
+function run_clock(id,endtime){
+    var clock = document.getElementById(id);
+    
+    // get spans where our clock numbers are held
+    var days_span = clock.querySelector('.days');
+    var hours_span = clock.querySelector('.hours');
+    var minutes_span = clock.querySelector('.minutes');
+    var seconds_span = clock.querySelector('.seconds');
+
+    function update_clock(){
+        var t = time_remaining(endtime);
+        
+        // update the numbers in each part of the clock
+        days_span.innerHTML = t.days;
+        hours_span.innerHTML = ('0' + t.hours).slice(-2);
+        minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
+        seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
+        
+        if(t.total<=0){ clearInterval(timeinterval); }
+    }
+    update_clock();
+    var timeinterval = setInterval(update_clock,1000);
+}
+run_clock('clockdiv',deadline);
+
