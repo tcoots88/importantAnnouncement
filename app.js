57e9edba0e111edbdf19e3ac2b10060a84cb0fcb
tcoots88 <travis.coots@yahoo.com>
2019-12-16 16:12:40 -0800

    initial3

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 'use strict'
+angular.module 'app', []
 
-var deadline = new Date("feb 12, 2020 00:00:01").getTime(); 
-  
-var x = setInterval(function() { 
-  
-var now = new Date().getTime(); 
-var t = deadline - now; 
-var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("day").innerHTML =days ; 
-document.getElementById("hour").innerHTML =hours; 
-document.getElementById("minute").innerHTML = minutes;  
-document.getElementById("second").innerHTML =seconds;  
-if (t < 0) { 
-        clearInterval(x); 
-        document.getElementById("demo").innerHTML = "TIME UP"; 
-        document.getElementById("day").innerHTML ='0'; 
-        document.getElementById("hour").innerHTML ='0'; 
-        document.getElementById("minute").innerHTML ='0' ;  
-        document.getElementById("second").innerHTML = '0'; } 
-}, 1000); 
+.directive 'countdown', ['Util', '$interval', (Util, $interval) ->
+  restrict: 'A'
+  scope:
+    date: '@'
+  link: (scope, element) ->    
+    future = new Date scope.date
+    $interval ->
+      diff = Math.floor (future.getTime() - new Date().getTime()) / 1000
+      element.text  Util.dhms diff
+    , 1000
+    return
+]
+
+.factory 'Util', [ ->
+  {
+    dhms: (t) ->
+      days = Math.floor t / 86400
+      t -= days * 86400
+      hours = Math.floor(t / 3600) % 24
+      t -= hours * 3600
+      minutes = Math.floor(t / 60) % 60
+      t -= minutes * 60
+      seconds = t % 60
+      [ days + 'd', hours + 'h', minutes + 'm', seconds + 's' ].join ' '
+  }
+]
